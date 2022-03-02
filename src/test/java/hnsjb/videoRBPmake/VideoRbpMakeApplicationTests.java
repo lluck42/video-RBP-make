441259e49306cf0a0a9132cdb90dfee44a30782e
lluck42 <lluck42@163.com>
2022-03-02 17:36:18 +0800

    init

---
+++ src/test/java/hnsjb/videoRBPmake/VideoRbpMakeApplicationTests.java
@@ -0,0 +1,13 @@
+package hnsjb.videoRBPmake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoRbpMakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
