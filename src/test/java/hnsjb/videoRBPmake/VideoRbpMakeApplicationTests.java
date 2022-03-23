35b1873c5cc093ed6cd7005b9a53c74111a85b18
lluck42 <lluck42@163.com>
2022-03-23 11:39:16 +0800

    测试关闭 防止打包时发送mail

--- src/test/java/hnsjb/videoRBPmake/VideoRbpMakeApplicationTests.java
+++ src/test/java/hnsjb/videoRBPmake/VideoRbpMakeApplicationTests.java
@@ -25,26 +25,27 @@ class VideoRbpMakeApplicationTests {
 
 	@Test
 	void sendMail() {
-        // 构建一个邮件对象
-        SimpleMailMessage message = new SimpleMailMessage();
-        // 设置邮件主题
-        message.setSubject("这是一封测试邮件");
-        // 设置邮件发送者，这个跟application.yml中设置的要一致
-        message.setFrom("82995820@qq.com");
-        // 设置邮件接收者，可以有多个接收者，中间用逗号隔开，以下类似
-      	// message.setTo("10*****16@qq.com","12****32*qq.com");
-        message.setTo("lluck42@163.com");
-        // 设置邮件抄送人，可以有多个抄送人
-        // message.setCc("12****32*qq.com");
-        // 设置隐秘抄送人，可以有多个
-        // message.setBcc("7******9@qq.com");
-        // 设置邮件发送日期
+        // return;
+        // // 构建一个邮件对象
+        // SimpleMailMessage message = new SimpleMailMessage();
+        // // 设置邮件主题
+        // message.setSubject("这是一封测试邮件");
+        // // 设置邮件发送者，这个跟application.yml中设置的要一致
+        // message.setFrom("82995820@qq.com");
+        // // 设置邮件接收者，可以有多个接收者，中间用逗号隔开，以下类似
+      	// // message.setTo("10*****16@qq.com","12****32*qq.com");
+        // message.setTo("lluck42@163.com");
+        // // 设置邮件抄送人，可以有多个抄送人
+        // // message.setCc("12****32*qq.com");
+        // // 设置隐秘抄送人，可以有多个
+        // // message.setBcc("7******9@qq.com");
+        // // 设置邮件发送日期
 		
-        message.setSentDate(new Date());
-        // 设置邮件的正文
-        message.setText("2022年3月23日这2是测试邮件\n的正文!!!");
-        // 发送邮件
-        javaMailSender.send(message);
+        // message.setSentDate(new Date());
+        // // 设置邮件的正文
+        // message.setText("2022年3月23日这2是测试邮件\n的正文!!!");
+        // // 发送邮件
+        // javaMailSender.send(message);
 	}
 	@Autowired
 	mail mail;
