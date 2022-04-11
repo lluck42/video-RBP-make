091f46076d5f336e14d75044dc1969c46fcc0f1f
lluck42 <lluck42@163.com>
2022-04-11 09:07:19 +0800

    debug

--- src/main/java/hnsjb/videoRBPmake/dao/form/companyMapper.java
+++ src/main/java/hnsjb/videoRBPmake/dao/form/companyMapper.java
@@ -15,7 +15,7 @@ import org.apache.ibatis.annotations.Update;
 @Mapper
 public interface companyMapper {
     
-    @Insert("insert into company(admin_id,name,address,shoot_num,clip_num,interview_num) values(#{admin_id},#{name},#{address},#{shoot_num},#{clip_num},#{interview_num})")
+    @Insert("insert into company(admin_id,name,address) values(#{admin_id},#{name},#{address})")
     @Options(useGeneratedKeys=true, keyProperty="id")
     int add(company one);
 
